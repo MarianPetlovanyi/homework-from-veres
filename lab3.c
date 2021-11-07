b1ec3ab3a47fb3b11bdbe7cdff8d785406e9f916
MarianPetlovanyi <93162348+MarianPetlovanyi@users.noreply.github.com>
2021-11-07 15:02:49 +0200

    Update lab3.c
    Implementation Moroz Anastasia's recommendations

--- lab3.c
+++ lab3.c
@@ -2,36 +2,36 @@
 #include "math.h"
 #include "stdio.h"
 int main(){
-    int minX, maxX, rowLength, newX;                    // variables
-    int *array1;                                        // my dynamically allocated array from minX to maxX, implemented below
-    scanf("%i%*c%i", &minX, &maxX);             // input minX and maxX
-    if (minX>=0 && maxX>=0)
-        rowLength=maxX-minX+1;
-    if (minX<0 && maxX>=0)
-        rowLength=abs(minX)+maxX+1;
-    if (minX<0 && maxX<=0)
-        rowLength=abs(minX)-abs(maxX)+1;
-    array1= malloc(rowLength*sizeof(int));         // mallocating array
-    for(int i=0; i<=rowLength; i++) {
-        newX=minX;                                      // newX is created to remember minX (like temp in swap) will be needed in line 25
-        redivide:if(abs(newX)%5==0 && abs(newX)%2==1){  // checking if number has 5(line 15-30) and removing
+    int beginOfArray, endOfArray, lengthOfArray, temp;                    // variables
+    int *numbers;                                        // my dynamically allocated array from beginOfArray to endOfArray, implemented below
+    scanf("%i%*c%i", &beginOfArray, &endOfArray);             // input beginOfArray and endOfArray
+    if (beginOfArray>=0 && endOfArray>=0)
+        lengthOfArray=endOfArray-beginOfArray+1;
+    if (beginOfArray<0 && endOfArray>=0)
+        lengthOfArray=abs(beginOfArray)+endOfArray+1;
+    if (beginOfArray<0 && endOfArray<=0)
+        lengthOfArray=abs(beginOfArray)-abs(endOfArray)+1;
+    numbers= malloc(lengthOfArray*sizeof(int));         // mallocating array
+    for(int i=0; i<=lengthOfArray; i++) {
+        temp=beginOfArray;                                      // temp is created to remember beginOfArray (like temp in swap) will be needed in line 25
+        redivide:if(abs(temp)%5==0 && abs(temp)%2==1){  // checking if number has 5(line 15-30) and removing
             i--;
-            rowLength--;                                // Veres I'm in history
-            minX++;                                     //_░▒███████
+            lengthOfArray--;                                // Veres I'm in history
+            beginOfArray++;                             //_░▒███████
         continue;                                       //░██▓▒░░▒▓██
     }                                                   //██▓▒░__░▒▓██___██████
     else{                                               //██▓▒░____░▓███▓__░▒▓██
-        while(abs(newX)>10){                            //██▓▒░___░▓██▓_____░▒▓██
-            newX=abs(newX)/10;                          //██▓▒░_______________░▒▓██
+        while(abs(temp)>10){                            //██▓▒░___░▓██▓_____░▒▓██
+            temp=abs(temp)/10;                          //██▓▒░_______________░▒▓██
             goto redivide;                              //_██▓▒░______IoT______░▒▓██
         }                                               //__██▓▒░_____________░▒▓██
-        array1[i]=minX;                                 //___██▓▒░__________░▒▓██
-        minX++;                                         //____██▓▒░________░▒▓██
+        numbers[i]=beginOfArray;                         //___██▓▒░__________░▒▓██
+        beginOfArray++;                                 //____██▓▒░________░▒▓██
     }                                                   //_____██▓▒░_____░▒▓██
     }                                                   //______██▓▒░__░▒▓██
-    for(int i=0; i<rowLength;i++ ){                     //_________░▒▓██
-        printf("%i ", array1[i]);               // printing array
+    for(int i=0; i<lengthOfArray;i++ ){                    //_________░▒▓██
+        printf("%i ", numbers[i]);               // printing array
     }
 
-    printf("=>%i ", rowLength);                 // printing the length of row
+    printf("=>%i ", lengthOfArray);                 // printing the length of row
 }
